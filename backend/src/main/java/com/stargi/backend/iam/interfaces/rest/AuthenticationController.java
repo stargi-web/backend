08148378014f16a47dcfb453dc4f4ba21ac2ffb8
FranchescoSoto <u201821078@upc.edu.pe>
2024-05-08 21:58:00 -0500

    Add controller

--- backend/src/main/java/com/stargi/backend/iam/interfaces/rest/AuthenticationController.java
+++ backend/src/main/java/com/stargi/backend/iam/interfaces/rest/AuthenticationController.java
@@ -1,10 +1,13 @@
 package com.stargi.backend.iam.interfaces.rest;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
+@RequestMapping(value = "/api/v1/auth", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthenticationController {
 
     @GetMapping()
